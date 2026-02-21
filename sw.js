2d24f56a527cf246db90824753a8b431649c9aeb
Pascoalwise01 <pascoalpinto01@gmail.com>
2026-02-21 10:03:50 +0100

    Add icon.png to cache and clean up event listeners

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="bakeboo-cache-v1";
-const urlsToCache=["index.html","style.css","script.js","manifest.json"];
+const CACHE_NAME = "bakeboo-cache-v1";
+const urlsToCache = ["index.html","style.css","script.js","manifest.json","icon.png"];
 
-self.addEventListener("install",(e)=>{
-    e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("fetch",(e)=>{
-    e.respondWith(caches.match(e.request).then(response=>response||fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
